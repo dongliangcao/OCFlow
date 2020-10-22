92208c16bacf134035a2a06654337ce427943472
Dongliang Cao <cao.dongliang97@gmail.com>
2020-10-22 15:08:00 +0200

    update warping layer

---
+++ models/networks/warping_layer.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Warping(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, img: torch.Tensor, flow: torch.Tensor) -> torch.Tensor:
+        """
+        warp I2 back to I1, according to the optical flow
+        
+        img: torch.Tensor [B, C, H, W]
+        flow: torch.Tensor [B, 2, H, W]
+        """
+        
+        B, C, H, W = img.size()
+        # create mesh grid
+        xx = torch.arange(0, W).view(1, -1).repeat(H, 1)
+        yy = torch.arange(0, H).view(-1, 1).repeat(1, W)
+        xx = xx.view(1, 1, H, W).repeat(B, 1, 1, 1)
+        yy = yy.view(1, 1, H, W).repeat(B, 1, 1, 1)
+        grid = torch.cat((xx, yy), 1).float()
+        # cast into cuda
+        if img.is_cuda:
+            grid = grid.cuda()
+        # require gradient
+        grid.requires_grad = True
+        vgrid = grid + flow
+        # scale grid to [-1, 1] to support grid_sample function in pytorch
+        # https://pytorch.org/docs/stable/nn.functional.html#grid-sample
+        vgrid[:,0,:,:] = 2.0 * vgrid[:,0,:,:].clone() / max(W-1, 1) - 1.0
+        vgrid[:,1,:,:] = 2.0 * vgrid[:,1,:,:].clone() / max(H-1, 1) - 1.0
+        # permute vgrid to size [B, H, W, 2] to support grid_sample function
+        vgrid = vgrid.permute(0, 2, 3, 1)
+        
+        output = F.grid_sample(img, vgrid)
+        
+        return output
+        
+        
