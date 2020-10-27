b69d05ec6815039fda74c9ecd22a77cb0318f5dc
TrungQuocNguyen <trungquoc.nguyen@tum.de>
2020-10-27 14:52:21 +0100

    Add Occlusion network

--- models/networks/flow_occ_net.py
+++ models/networks/flow_occ_net.py
@@ -33,6 +33,33 @@ class OpticalFlowEstimator(nn.Module):
             flow_up = self.upconv1(w_lev)
             feature_up = self.upconv2(f_lev)
             return(w_lev, flow_up, feature_up)
+class OcclusionEstimator(nn.Module): 
+    def __init__(self, input_channels, level, highest_resolution = False):
+        super(OcclusionEstimator, self).__init__()
+        self.highest_res = highest_resolution
+        self.conv1 = nn.Conv2d(input_channels, 128, 3, 1, 1)
+        self.conv2 = nn.Conv2d(128, 96, 3, 1, 1)
+        self.conv3 = nn.Conv2d(96, 64, 3, 1, 1)
+        self.conv4 = nn.Conv2d(64, 32, 3, 1, 1)
+        self.feat_layer = nn.Conv2d(32, 16, 3, 1, 1)
+        self.mask_layer = nn.Conv2d(16, 1, 3, 1, 1)
+
+        self.upconv1 = nn.ConvTranspose2d(16, 1, 4, 2, 1)
+        self.upconv2 = nn.ConvTranspose2d(1, 1, 4, 2, 1)
+    def forward(self, features):
+        c1 = F.leaky_relu(self.conv1(features), negative_slope= 0.1)
+        c2 = F.leaky_relu(self.conv2(c1), negative_slope= 0.1)
+        c3 = F.leaky_relu(self.conv3(c2), negative_slope= 0.1)
+        c4 = F.leaky_relu(self.conv4(c3), negative_slope= 0.1)
+        feat = F.leaky_relu(self.feat_layer(c4), negative_slope= 0.1)
+        occ_mask = F.sigmoid(self.mask_layer(feat))
+        if self.highest_res: 
+            return occ_mask
+        else: 
+            features_up = F.sigmoid(upconv1(feat))
+            occ_mask_up = F.sigmoid(upconv2(occ_mask))
+            return (occ_mask, features_up, occ_mask_up)
+
 
 
 
