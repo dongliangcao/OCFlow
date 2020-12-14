269b0957cbf0c567c9739c91e8454779f8176fff
TrungQuocNguyen <trungquoc.nguyen@tum.de>
2020-12-14 15:43:38 +0100

    Update training process

--- test_unsupervised.py
+++ test_unsupervised.py
@@ -1,5 +1,5 @@
 import pytorch_lightning as pl
-from models.model import FlowStageModel, InpaintingStageModel
+from models.model import FlowStageModel, InpaintingStageModel, TwoStageModel, TwoStageModelGC
 from models.lightning_datamodule import DatasetModule
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning import loggers as pl_loggers
@@ -9,8 +9,8 @@ import torch
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('--network_type', type=str, help='Type of network: [flow, inpainting]', default='flow')
+    #parameters for all models
+    parser.add_argument('--network_type', type=str, help='Type of network: [flow, inpainting, twostage]', default='flow')
     parser.add_argument('--model', type=str, help='Type of model', default='simple')
     parser.add_argument('--dataset_name', type=str, help='Name of dataset', default = 'MpiSintelClean')
     parser.add_argument('--root', type=str, help='Data root')
@@ -20,23 +20,44 @@ if __name__ == '__main__':
     parser.add_argument('--learning_rate', type=float, help='Learning rate', default=1e-3) 
     parser.add_argument('--overfit_batches', type=int, help='Mode of training', default =0)
     parser.add_argument('--find_best_lr', action = 'store_true', help='Use Trainer to find the best learning')
+    parser.add_argument('--log_every_n_steps', type=int, help='Log every n steps', default = 20)
+
+    #parameters for each model
+    parser.add_argument('--smoothness_weight', type=float, help='Weight for smoothness loss', default=0.05)
+    parser.add_argument('--second_order_weight', type=float, help='Weight for gradient of image', default=0.0)
+    parser.add_argument('--reconst_weight', type=float, help='Weight for reconstruction loss', default=2.0)
+    parser.add_argument('--with_occ', action = 'store_true', help='Training flownet with ground truth occlusion mask or not')
+    parser.add_argument('--flow_root', type=str, help='Path to flow model checkpoint', default=None)
+    parser.add_argument('--inpainting_root', type=str, help='Path to inpainting model checkpoint', default=None)
+    
 
 
     args = parser.parse_args()
     
-    hparams = dict(network_type = args.network_type, model=args.model, epochs = args.epochs, batch_size=args.batch_size, learning_rate=args.learning_rate)
+    hparams = dict(network_type = args.network_type, model=args.model, epochs = args.epochs, batch_size=args.batch_size, learning_rate=args.learning_rate, log_every_n_steps = args.log_every_n_steps)
 
     network_type = args.network_type
-    assert network_type in ['flow', 'inpainting'], 'Unknown network type'
+    assert network_type in ['flow', 'inpainting', 'twostage'], 'Unknown network type'
     if network_type == 'flow':
         assert hparams['model'] in ['simple', 'flownets', 'flownetc', 'pwc', 'flownet', 'eflownet', 'eflownet2']
-        hparams['smoothness_weight'] = 0.05
-        hparams['second_order_weight'] = 0.0
-        hparams['with_occ'] = False
+        hparams['smoothness_weight'] = args.smoothness_weight
+        hparams['second_order_weight'] = args.second_order_weight
+        hparams['with_occ'] = args.with_occ
         model = FlowStageModel(hparams=hparams)
-    else:
-        hparams['second_order_weight'] = 0.0
+    elif network_type == 'inpainting':
+        hparams['second_order_weight'] = args.second_order_weight
         model = InpaintingStageModel(hparams=hparams)
+    else: 
+        assert hparams['model'] in ['with_gt_flow', 'no_gt_flow']
+        hparams['reconst_weight'] = args.reconst_weight
+        hparams['inpainting_root'] = args.inpainting_root
+        if hparams['model'] == 'no_gt_flow': 
+            hparams['smoothness_weight'] = args.smoothness_weight
+            hparams['flow_root'] = args.flow_root
+            model = TwoStageModel(hparams=hparams)
+        else: 
+            model = TwoStageModelGC(hparams=hparams)
+
     max_epochs = args.epochs
     #specify data module
     dataset_name = args.dataset_name
