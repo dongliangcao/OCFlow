81887aa9471172a369835fee92617f5e41abd413
Dongliang Cao <cao.dongliang97@gmail.com>
2020-12-23 08:24:03 +0100

    update tests

--- test.py
+++ test.py
@@ -8,29 +8,34 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import ModelCheckpoint
 import argparse
+import yaml
 import torch
 # flow occ model should be ['simple', 'flowoccnets', 'flowoccnetc', 'pwoc', 'flowoccnet']
 # optical flow model should be ['simple', 'flownets', 'flownetc', 'pwc', 'flownet']
 # occlusion model should be ['simple', 'occnets', 'occnetc']
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+#     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--network_type', type=str, help='Type of network: [flow, occ, flow-occ, inpainting]', default='flow')
-    parser.add_argument('--model', type=str, help='Type of model', default='simple')
-    parser.add_argument('--epochs', type=int, help='Epochs to train', default=100)
-    parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
-    parser.add_argument('--learning_rate', type=float, help='Learning rate', default=1e-3) 
-    parser.add_argument('--dataset_name', type=str, help='Name of dataset', default = 'MpiSintelClean')
-    parser.add_argument('--root', type=str, help='Data root')
-    parser.add_argument('--overfit_batches', type=int, help='Mode of training', default =0)
-    parser.add_argument('--find_best_lr', action = 'store_true', help='Use Trainer to find the best learning')
+#     parser.add_argument('--network_type', type=str, help='Type of network: [flow, occ, flow-occ, inpainting]', default='flow')
+#     parser.add_argument('--model', type=str, help='Type of model', default='simple')
+#     parser.add_argument('--epochs', type=int, help='Epochs to train', default=100)
+#     parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
+#     parser.add_argument('--learning_rate', type=float, help='Learning rate', default=1e-3) 
+#     parser.add_argument('--dataset_name', type=str, help='Name of dataset', default = 'MpiSintelClean')
+#     parser.add_argument('--root', type=str, help='Data root')
+#     parser.add_argument('--overfit_batches', type=int, help='Mode of training', default =0)
+#     parser.add_argument('--find_best_lr', action = 'store_true', help='Use Trainer to find the best learning')
                         
-    args = parser.parse_args()
+#     args = parser.parse_args()
     
-    hparams = dict(network_type = args.network_type, model=args.model, epochs = args.epochs, batch_size=args.batch_size, learning_rate=args.learning_rate)
+    file_name = r'config\supervised_config.yml'
+    with open(file_name) as f:
+        args = yaml.load(f, Loader=yaml.FullLoader)
     
-    network_type = args.network_type
+    hparams = dict(network_type = args['network_type'], model=args['model'], epochs = args['epochs'], batch_size=args['batch_size'], learning_rate=args['learning_rate'])
+    
+    network_type = args['network_type']
     assert network_type in ['flow', 'occ', 'flow-occ', 'inpainting'], 'Unknown network type'
     if network_type == 'flow':
         assert hparams['model'] in ['simple', 'flownets', 'flownetc', 'pwc', 'flownet', 'eflownet', 'eflownet2']
@@ -44,12 +49,12 @@ if __name__ == '__main__':
     else:
         model = InpaintingModel(hparams=hparams)
     
-    max_epochs = args.epochs
+    max_epochs = args['epochs']
     #specify data module
-    dataset_name = args.dataset_name
-    image_size = (64,128)
+    dataset_name = args['dataset_name']
+    image_size = args['image_size']
     assert dataset_name in ['ImgFlowOcc', 'MpiSintelClean', 'MpiSintelFinal', 'MpiSintelCleanOcc', 'MpiSintelFinalOcc', 'MpiSintelCleanFlowOcc', 'MpiSintelFinalFlowOcc']
-    data_module = DatasetModule(root=args.root,image_size= image_size, batch_size=args.batch_size, dataset_name=dataset_name)
+    data_module = DatasetModule(root=args['root'],image_size=image_size, batch_size=args['batch_size'], dataset_name=dataset_name)
     data_module.prepare_data()
     data_module.setup()
     #specify early stopping callbacks
@@ -66,14 +71,14 @@ if __name__ == '__main__':
     #specify logger
     tb_logger = pl_loggers.TensorBoardLogger('tensorboard_logs/')
     #specify Trainer and start training
-    if not args.find_best_lr: 
-        trainer = pl.Trainer(max_epochs=max_epochs, gpus=1, overfit_batches=args.overfit_batches, logger = tb_logger, callbacks =[checkpoint_callback])
-        trainer.fit(model, datamodule = data_module)
+    if not args['find_best_lr']: 
+        trainer = pl.Trainer(max_epochs=max_epochs, gpus=1, overfit_batches=args['overfit_batches'], logger=tb_logger, callbacks=[checkpoint_callback])
+        trainer.fit(model, datamodule=data_module)
     else: 
-        trainer = pl.Trainer(gpus =1, max_epochs = max_epochs, logger = tb_logger, callbacks =[checkpoint_callback])
-        lr_finder = trainer.tuner.lr_find(model, datamodule = data_module, early_stop_threshold=None, num_training=100)
+        trainer = pl.Trainer(gpus=1, max_epochs=max_epochs, logger=tb_logger, callbacks=[checkpoint_callback])
+        lr_finder = trainer.tuner.lr_find(model, datamodule=data_module, early_stop_threshold=None, num_training=100)
         suggested_lr = lr_finder.suggestion()
         print(suggested_lr)
-        model.hparams['lr'] = suggested_lr
-        trainer.fit(model, datamodule = data_module)
+        model.lr = suggested_lr
+        trainer.fit(model, datamodule=data_module)
     print(trainer.logged_metrics)
--- test_unsupervised.py
+++ test_unsupervised.py
@@ -5,64 +5,69 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import ModelCheckpoint
 import argparse
+import yaml
 import torch
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    #parameters for all models
-    parser.add_argument('--network_type', type=str, help='Type of network: [flow, inpainting, twostage]', default='flow')
-    parser.add_argument('--model', type=str, help='Type of model', default='simple')
-    parser.add_argument('--dataset_name', type=str, help='Name of dataset', default = 'MpiSintelClean')
-    parser.add_argument('--root', type=str, help='Data root')
+#     parser = argparse.ArgumentParser()
+#     #parameters for all models
+#     parser.add_argument('--network_type', type=str, help='Type of network: [flow, inpainting, twostage]', default='flow')
+#     parser.add_argument('--model', type=str, help='Type of model', default='simple')
+#     parser.add_argument('--dataset_name', type=str, help='Name of dataset', default = 'MpiSintelClean')
+#     parser.add_argument('--root', type=str, help='Data root')
     
-    parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
-    parser.add_argument('--epochs', type=int, help='Epochs to train', default=100)
-    parser.add_argument('--learning_rate', type=float, help='Learning rate', default=1e-3) 
-    parser.add_argument('--overfit_batches', type=int, help='Mode of training', default =0)
-    parser.add_argument('--find_best_lr', action='store_true', help='Use Trainer to find the best learning')
-    parser.add_argument('--log_every_n_steps', type=int, help='Log every n steps', default = 20)
-    parser.add_argument('--image_size', type=int, nargs=2, help='Image size', default=[64, 128])
-    #parameters for each model
-    parser.add_argument('--smoothness_weight', type=float, help='Weight for smoothness loss', default=0.05)
-    parser.add_argument('--second_order_weight', type=float, help='Weight for gradient of image', default=0.0)
-    parser.add_argument('--reconst_weight', type=float, help='Weight for reconstruction loss', default=2.0)
-    parser.add_argument('--with_occ', action='store_true', help='Training flownet with ground truth occlusion mask or not')
-    parser.add_argument('--flow_root', type=str, help='Path to flow model checkpoint', default=None)
-    parser.add_argument('--supervised_flow', action='store_true', help='Used trained supervised model in TwoStageModel instead of unsupervised one')
-    parser.add_argument('--inpainting_root', type=str, help='Path to inpainting model checkpoint', default=None)
-    parser.add_argument('--org', action='store_true', help='Use the original version of the gated convolution')
+#     parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
+#     parser.add_argument('--epochs', type=int, help='Epochs to train', default=100)
+#     parser.add_argument('--learning_rate', type=float, help='Learning rate', default=1e-3) 
+#     parser.add_argument('--overfit_batches', type=int, help='Mode of training', default =0)
+#     parser.add_argument('--find_best_lr', action='store_true', help='Use Trainer to find the best learning')
+#     parser.add_argument('--log_every_n_steps', type=int, help='Log every n steps', default = 20)
+#     parser.add_argument('--image_size', type=int, nargs=2, help='Image size', default=[64, 128])
+#     #parameters for each model
+#     parser.add_argument('--smoothness_weight', type=float, help='Weight for smoothness loss', default=0.05)
+#     parser.add_argument('--second_order_weight', type=float, help='Weight for gradient of image', default=0.0)
+#     parser.add_argument('--reconst_weight', type=float, help='Weight for reconstruction loss', default=2.0)
+#     parser.add_argument('--with_occ', action='store_true', help='Training flownet with ground truth occlusion mask or not')
+#     parser.add_argument('--flow_root', type=str, help='Path to flow model checkpoint', default=None)
+#     parser.add_argument('--supervised_flow', action='store_true', help='Used trained supervised model in TwoStageModel instead of unsupervised one')
+#     parser.add_argument('--inpainting_root', type=str, help='Path to inpainting model checkpoint', default=None)
+#     parser.add_argument('--org', action='store_true', help='Use the original version of the gated convolution')
 
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
     
-    hparams = dict(network_type = args.network_type, model=args.model, epochs = args.epochs, batch_size=args.batch_size, learning_rate=args.learning_rate, log_every_n_steps = args.log_every_n_steps, img_size=args.image_size, org=args.org)
+    file_name = r'config\unsupervised_config.yml'
+    with open(file_name) as f:
+        args = yaml.load(f, Loader=yaml.FullLoader)
+    
+    hparams = dict(network_type = args['network_type'], model=args['model'], epochs = args['epochs'], batch_size=args['batch_size'], learning_rate=args['learning_rate'], log_every_n_steps = args['log_every_n_steps'], img_size=args['image_size'], org=args['org'])
 
-    network_type = args.network_type
+    network_type = args['network_type']
     automatic_optimization = True
-    image_size = args.image_size
+    image_size = args['image_size']
     assert network_type in ['flow', 'inpainting', 'twostage'], 'Unknown network type'
     if network_type == 'flow':
         assert hparams['model'] in ['simple', 'flownets', 'flownetc', 'pwc', 'flownet', 'eflownet', 'eflownet2']
-        hparams['smoothness_weight'] = args.smoothness_weight
-        hparams['second_order_weight'] = args.second_order_weight
-        hparams['with_occ'] = args.with_occ
+        hparams['smoothness_weight'] = args['smoothness_weight']
+        hparams['second_order_weight'] = args['second_order_weight']
+        hparams['with_occ'] = args['with_occ']
         model = FlowStageModel(hparams=hparams)
     elif network_type == 'inpainting':
         assert hparams['model'] in ['simple', 'gated']
         if hparams['model'] == 'simple': 
-            hparams['second_order_weight'] = args.second_order_weight
+            hparams['second_order_weight'] = args['second_order_weight']
             model = InpaintingStageModel(hparams=hparams)
         else: 
-            model = InpaintingGConvModel(hparams = hparams)
+            model = InpaintingGConvModel(hparams=hparams)
             automatic_optimization = False
     else: 
         assert hparams['model'] in ['with_gt_flow', 'no_gt_flow']
-        hparams['reconst_weight'] = args.reconst_weight
-        hparams['inpainting_root'] = args.inpainting_root
+        hparams['reconst_weight'] = args['reconst_weight']
+        hparams['inpainting_root'] = args['inpainting_root']
         if hparams['model'] == 'no_gt_flow': 
-            hparams['smoothness_weight'] = args.smoothness_weight
-            hparams['flow_root'] = args.flow_root
-            if args.supervised_flow: 
+            hparams['smoothness_weight'] = args['smoothness_weight']
+            hparams['flow_root'] = args['flow_root']
+            if args['supervised_flow']: 
                 hparams['supervised_flow'] = True
             else: 
                 hparams['supervised_flow'] = False
@@ -70,12 +75,12 @@ if __name__ == '__main__':
         else: 
             model = TwoStageModelGC(hparams=hparams)
 
-    max_epochs = args.epochs
+    max_epochs = args['epochs']
     #specify data module
-    dataset_name = args.dataset_name
+    dataset_name = args['dataset_name']
     
     assert dataset_name in ['ImgFlowOcc', 'MpiSintelClean', 'MpiSintelFinal', 'MpiSintelCleanOcc', 'MpiSintelFinalOcc', 'MpiSintelCleanFlowOcc', 'MpiSintelFinalFlowOcc', 'MpiSintelCleanInpainting', 'MpiSintelFinalInpainting', 'FlyingChairsInpainting']
-    data_module = DatasetModule(root=args.root,image_size= image_size, batch_size=args.batch_size, dataset_name=dataset_name)
+    data_module = DatasetModule(root=args['root'],image_size=image_size, batch_size=args['batch_size'], dataset_name=dataset_name, static_occ=args['static_occ'])
     data_module.prepare_data()
     data_module.setup()
     #specify early stopping callback
@@ -92,8 +97,8 @@ if __name__ == '__main__':
     #specify logger
     tb_logger = pl_loggers.TensorBoardLogger('tensorboard_logs/')
     #specify Trainer and start training
-    if not args.find_best_lr: 
-        trainer = pl.Trainer(max_epochs=max_epochs, gpus=1, overfit_batches=args.overfit_batches, logger=tb_logger, callbacks=[checkpoint_callback], automatic_optimization=automatic_optimization)
+    if not args['find_best_lr']: 
+        trainer = pl.Trainer(max_epochs=max_epochs, gpus=1, overfit_batches=args['overfit_batches'], logger=tb_logger, callbacks=[checkpoint_callback], automatic_optimization=automatic_optimization)
         trainer.fit(model, datamodule=data_module)
     else: 
         trainer = pl.Trainer(gpus=1, max_epochs=max_epochs, logger=tb_logger, callbacks=[checkpoint_callback], automatic_optimization= automatic_optimization)
